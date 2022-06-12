44aaf50864c0e907f52bb18af236e8d0091b5af4
kamilsamaj <kamilsamaj@gmail.com>
2022-06-12 19:18:31 +0200

    initial commit

---
+++ src/test/java/com/ks/udemyspringboot/UdemySpringbootApplicationTests.java
@@ -0,0 +1,13 @@
+package com.ks.udemyspringboot;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class UdemySpringbootApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
